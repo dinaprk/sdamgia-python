c1200a909563d64ea4b7a5293b6f53ae26e49390
Динаzavr <dinaprk@cumallover.me>
2024-03-31 17:20:27 +0300

    Rewrite html parsing in selectolax

--- sdamgia/api.py
+++ sdamgia/api.py
@@ -9,10 +9,10 @@ from typing import Any, Literal, Self
 from urllib.parse import urljoin
 
 import aiohttp
-import bs4
 from cairosvg import svg2png
 from PIL import Image
 from PIL.Image import Image as ImageType
+from selectolax.parser import HTMLParser, Node
 
 from .types import BASE_DOMAIN, GitType, Problem, ProblemPart, Subject
 
@@ -61,41 +61,42 @@ class SdamgiaAPI:
         problem_id: int,
         recognize_text: bool = False,
     ) -> Problem:
-        soup = self._soup(await self._get(f"/problem?id={problem_id}"))
+        parser = HTMLParser(await self._get(f"/problem?id={problem_id}"))
 
-        if (problem_tag := soup.find("div", class_="prob_maindiv")) is None:
-            raise RuntimeError("Problem tag not found")
+        if (problem_node := parser.css_first(".prob_maindiv")) is None:
+            raise RuntimeError("Problem node not found")
 
         # make all image urls absolute
-        for img_tag in problem_tag.find_all("img"):
-            if BASE_DOMAIN not in img_tag["src"]:
-                img_tag["src"] = urljoin(self.base_url, img_tag["src"])
+        for img_node in problem_node.css("img"):
+            if BASE_DOMAIN not in (url := str(img_node.attributes["src"])):
+                img_node.attributes["src"] = urljoin(self.base_url, url)
 
         try:
-            topic_id = int(problem_tag.find("span", class_="prob_nums").text.split()[1])
+            topic_id = int(problem_node.css_first("span.prob_nums").text().split()[1])
         except (IndexError, AttributeError, ValueError):
             topic_id = None
 
         try:
-            condition_tag = soup.find("div", class_="pbody")
-            condition = await self._get_problem_part(condition_tag, recognize_text=recognize_text)
+            condition_node = parser.css_first("div.pbody")
+            condition = await self._get_problem_part(condition_node, recognize_text=recognize_text)
         except (IndexError, AttributeError):
             condition = None
 
         try:
-            if (solution_tag := problem_tag.find("div", class_="solution")) is None:
-                solution_tag = problem_tag.find_all("div", class_="pbody")[1]
-            solution = await self._get_problem_part(solution_tag, recognize_text=recognize_text)
+            solution_node = (
+                problem_node.css_first("div.solution") or problem_node.css("div.pbody")[1]
+            )
+            solution = await self._get_problem_part(solution_node, recognize_text=recognize_text)
         except (IndexError, AttributeError):
             solution = None
 
         try:
-            answer = problem_tag.find("div", class_="answer").text.lstrip("Ответ:").strip()
+            answer = problem_node.css_first("div.answer").text().lstrip("Ответ:").strip()
         except (IndexError, AttributeError):
             answer = ""
 
         analog_urls = [
-            link.get("href") for link in problem_tag.find("div", class_="minor").find_all("a")
+            str(link.attributes["href"]) for link in problem_node.css_first("div.minor").css("a")
         ]
         analog_ids = [
             int(mo.group(1)) for url in analog_urls if (mo := re.search(r"id=(\d+)", url))
@@ -124,23 +125,21 @@ class SdamgiaAPI:
     @_handle_params
     async def get_test(self, test_id: int) -> list[int]:
         """Получение списка задач, включенных в тест"""
-        soup = self._soup(await self._get(f"/test?id={test_id}"))
-        return self._get_problem_ids(soup)
+        parser = HTMLParser(await self._get(f"/test?id={test_id}"))
+        return self._get_problem_ids(parser)
 
     @_handle_params
     async def get_catalog(self) -> list[dict[str, Any]]:
         """Получение каталога заданий для определенного предмета"""
-        soup = self._soup(await self._get("/prob_catalog"))
-        topics = [
-            c for c in soup.find_all("div", class_="cat_category") if c.get("data-id") is None
-        ]
+        parser = HTMLParser(await self._get("/prob_catalog"))
+        topics = [c for c in parser.css("div.cat_category") if c.attributes.get("data-id") is None]
         topics = topics[1:]  # skip header
 
         catalog = []
         for topic in topics:
-            topic_id, topic_name = topic.find("b", class_="cat_name").text.split(". ", maxsplit=1)
-            additional = "д" in topic_id.lower()
-            topic_id = int(re.search(r"\d+", topic_id).group())  # type: ignore[union-attr]
+            topic_id_str, topic_name = topic.css_first("b.cat_name").text().split(". ", maxsplit=1)
+            additional = "д" in topic_id_str.lower()
+            topic_id = int(re.search(r"\d+", topic_id_str).group())  # type: ignore[union-attr]
 
             catalog.append(
                 {
@@ -149,12 +148,10 @@ class SdamgiaAPI:
                     "additional": additional,
                     "categories": [
                         {
-                            "category_id": int(cat_tag.get("data-id")),
-                            "category_name": cat_tag.find("a", class_="cat_name").text,
+                            "category_id": int(cat_node.attributes["data-id"]),  # type: ignore[arg-type]
+                            "category_name": cat_node.css_first("a.cat_name").text(),
                         }
-                        for cat_tag in topic.find("div", class_="cat_children").find_all(
-                            "div", class_="cat_category"
-                        )
+                        for cat_node in topic.css_first("div.cat_children").css("div.cat_category")
                     ],
                 }
             )
@@ -275,10 +272,6 @@ class SdamgiaAPI:
             response.raise_for_status()
             return await response.text()
 
-    @staticmethod
-    def _soup(html: str) -> bs4.BeautifulSoup:
-        return bs4.BeautifulSoup(markup=html, features="lxml")
-
     async def _fetch_svg(self, url: str) -> ImageType:
         byte_string = await self._get(url=url)
         png_bytes = svg2png(bytestring=byte_string)
@@ -295,40 +288,40 @@ class SdamgiaAPI:
                 raise RuntimeError("'pix2tex' is required for this functional but not found")
         return f"${self._latex_ocr_model(image)}$"  # type: ignore[misc]
 
-    async def _get_problem_part(self, tag: bs4.Tag, recognize_text: bool = False) -> ProblemPart:
-        image_tags = tag.find_all("img", class_="tex")
-        image_urls = [img_tag.get("src") for img_tag in image_tags]
+    async def _get_problem_part(self, node: Node, recognize_text: bool = False) -> ProblemPart:
+        image_nodes = node.css("img.tex")
+        image_urls = [str(img_node.attributes["src"]) for img_node in image_nodes]
 
         if recognize_text:
             images = await asyncio.gather(
                 *[asyncio.create_task(self._fetch_svg(url)) for url in image_urls]
             )
 
-            for img_tag, image in zip(image_tags, images):
-                img_tag.replace_with(self._recognize_image_text(image))
+            for img_node, image in zip(image_nodes, images):
+                img_node.replace_with(self._recognize_image_text(image))
 
-            text = tag.get_text(strip=True).replace("−", "-").replace("\xad", "")
-            text = unicodedata.normalize("NFKC", text)
+            text = node.text(strip=True, deep=True)
+            text = unicodedata.normalize("NFKC", text).replace("\xad", "")
         else:
             text = ""
 
-        for img_tag in tag.find_all("img"):
-            if (url := img_tag.get("src")) not in image_urls:
+        for img_node in node.css("img"):
+            if (url := str(img_node.attributes["src"])) not in image_urls:
                 image_urls.append(url)
 
-        return ProblemPart(text=text, html=str(tag), image_urls=image_urls)
+        return ProblemPart(text=text, html=str(node.html), image_urls=image_urls)
 
     @staticmethod
-    def _get_problem_ids(tag: bs4.Tag) -> list[int]:
-        return [int(span.find("a").text) for span in tag.find_all("span", class_="prob_nums")]
+    def _get_problem_ids(node: Node | HTMLParser) -> list[int]:
+        return [int(node.css_first("a").text()) for node in node.css("span.prob_nums")]
 
     async def _get_problem_ids_pagination(self, path: str, params: dict[str, Any]) -> list[int]:
         result: list[int] = []
         page = 1
         while True:
             params |= {"page": page}
-            soup = self._soup(await self._get(path, params=params))
-            if not (ids := self._get_problem_ids(soup)):
+            parser = HTMLParser(await self._get(path, params=params))
+            if not (ids := self._get_problem_ids(parser)):
                 return result
             for id in ids:
                 # to prevent bug when site infinitely returns last results page
